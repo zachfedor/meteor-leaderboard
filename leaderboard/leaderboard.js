d456949882d8e4f212331a909ac838b108172018
Zach Fedor <fedor@towermarketing.net>
2015-03-26 17:16:00 -0400

    first commit

---
+++ leaderboard/leaderboard.js
@@ -0,0 +1,14 @@
+/**
+ * Leaderboard
+ *
+ * author: zachfedor
+ * url: https://github.com/zachfedor/meteor-leaderboard
+ * license: MIT
+ */
+
+/**
+ * MongoDB
+ */
+PlayersList = new Mongo.Collection('players');
+
+//PlayersList.insert();
